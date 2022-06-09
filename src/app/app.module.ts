a86727c7c05181cde76c2f4ec4cf3c0449545542
gillemta <tashan.gillem@gmail.com>
2022-06-08 23:44:35 -0400

    Setup portfolio foundation and added Navbar,Home Page, and Bio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './home/header/header.component';
+import { BioComponent } from './home/bio/bio.component';
+import { WorkComponent } from './home/work/work.component';
+import { SkillsComponent } from './home/skills/skills.component';
+import { ProjectsComponent } from './home/projects/projects.component';
+import { ContactComponent } from './home/contact/contact.component';
+import { CoverComponent } from './home/cover/cover.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    BioComponent,
+    WorkComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ContactComponent,
+    CoverComponent
   ],
   imports: [
     BrowserModule
